153a328bfe38bddd2123e8d720c30416d4808963
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-03-28 22:19:10 -0400

    I came to the realization that committing often is good. I did so much to this code there is no way to know where I should roll back to. I added functionality to the buttons, created logic for button generation, added multiple new files, and much more

--- assets/questions.js
+++ assets/questions.js
@@ -1,11 +1,32 @@
 var questions = [{
 
     question: 'Inside which HTML element do we put the JavaScript?',
-    options: ['<js>', '<javascript>', '<script>', '<scripting>'],
+    options: ['<javascript>', '<script>', '<scripting>'],
     answer: '<script>'
 },
 {
     question: 'Where is the correct place to insert a JavaScript?',
-    options: ['The <head> section', 'The <body> section', 'Both the <head> and <body> section'],
+    options: ['The <body> section', 'Both the <head> and <body> section'],
     answer: 'The <body> section'
-}]
+},
+{
+    question: 'What does HTML stand for?',
+    options: ['Hyperlinks and Text Markup Language', 'Hyper Text Markup Language', 'Home Tool Markup Language'],
+    answer: 'Hyper Text Markup Language'
+},
+{   
+    question: 'Choose the correct HTML tag for the largest heading',
+    options: ['<h6>', '<head>','<h1>'],
+    answer: '<h1>'
+},
+{
+    question: 'The external JavaScript file must contain the <script> tag.',
+    options: ['True','False','Both True and False'],
+    answer: 'False'
+},
+{
+    question: 'JavaScript is the same as Java.',
+    options: ['True','False','Both True and False'],
+    answer: 'False'
+}
+]
--- assets/script.js
+++ assets/script.js
@@ -1,98 +1,170 @@
-//Timer element
-var timerEl = document.getElementById('timer');
-
-//start button element
-var startButton = document.getElementById('start-button');
-
-//quiz container
-var quizBody = document.getElementById('quiz-box');
-
-//start Screen element
-var startScreen = document.getElementById('start-screen');
-
-//question screen element
-var qScreen = document.getElementById('q-screen');
-
-//question element in question screen
-var qQuestion = document.getElementById('question');
-
-// choices element in question screen
-var qChoices = document.getElementById('choices');
-
-//end screen element
-var endScreen = document.getElementById('end-screen');
-
-//hide screen
-var hideScreen = document.getElementsByClassName('hide');
-
-//index of current question
+//Int Vars
+var timeLeft = 15;
 var questionIndex = 0;
+var score = 0;
+
+//Get elements by ID
+var timerEl = document.getElementById("timer");
+var startButton = document.getElementById("start-button");
+var quizBody = document.getElementById("quiz-box");
+var startScreen = document.getElementById("start-screen");
+var qScreen = document.getElementById("q-screen");
+var qQuestion = document.getElementById("question");
+var qChoices = document.getElementById("choices");
+var endScreen = document.getElementById("end-screen");
+var scoreMessage = document.getElementById("score-message");
+var feedbackArea = document.getElementById("feedback-area");
+var submitButton = document.getElementById("submit");
+var initialsEl = document.getElementById("initials");
+
+//get Element that hides windows
+var hideScreen = document.getElementsByClassName("hide");
 
 //creates header and div elements
-var h2El = document.createElement('h2');
-var divEl = document.createElement('div');
+var pEl = document.createElement("p");
+var h2El = document.createElement("h2");
+var divEl = document.createElement("div");
 
 //creates button elements for questions
-var q1 = document.createElement('button');
-var q2 = document.createElement('button');
-var q3 = document.createElement('button');
-var q4 = document.createElement('button');
+var q1 = document.createElement("button");
+var q2 = document.createElement("button");
+var q3 = document.createElement("button");
+var q4 = document.createElement("button");
 
 //creates an array of button creator elements
-var q = [q1, q2,q3, q4];
+var q = [q1, q2, q3, q4];
 
 
 //startButton event Listener
-    startButton.addEventListener("click", function(){
-    qScreen.removeAttribute("class", "hide");
-    startScreen.setAttribute("class", "hide");
+startButton.addEventListener("click", function () {
+  qScreen.removeAttribute("class", "hide");
+  startScreen.setAttribute("class", "hide");
 
-    countdown();
-    getQuestion();
+  countdown();
+  getQuestion();
+});
+
+//adds functionality to the submit button
+submitButton.addEventListener("click", function(){
+    var initials = initialsEl.value;
+
+    //parses the highscores object in local storage
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    //creates an object for the new score
+    var newScore = {
+        userScore: score,
+        userInitials: initials
+    }
+
+    //pushes the new score object to the highscores object
+    highscores.push(newScore);
+
+    //sets the updated array to local Storage
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    //changes to the highScores.html page
+    window.location.href = "highScores.html";
 });
 
 //getQuestion function - retrieves a question from the questions object
 function getQuestion() {
-    //sets current question to question index
-    var currentQuestion = questions[questionIndex];
-    console.log(questions[questionIndex].question);
-    
 
-    //sets h2 question to question value in questions object
-    qQuestion.textContent = currentQuestion.question;
-    
-    //appends the questions to the question header element
-    qQuestion.appendChild(h2El);
+  //Finishes game if all questions have been answered
+  if (questionIndex === Object.keys(questions).length) {
+    qScreen.setAttribute("class", "hide");
+    endScreen.removeAttribute("class", "hide");
+    console.log("Game finished");
+    questionIndex = 0;
+    score = timeLeft;
+    scoreMessage.textContent = "Your Final score is " + score;
+    timeLeft = 0;
+  }
 
-    //creates a current options array
-    var currentOptions = questions[1].options;
+  //sets current question to question index
+  var currentQuestion = questions[questionIndex];
+  //creates a current options array
+  var currentOptions = questions[questionIndex].options;
 
-    //creates a button for each question
-    for(var i = 0; i < currentOptions.length; i++){
-        q[i].textContent = i + 1 + ": " + currentOptions[i];
-        qChoices.appendChild(q[i]);
-        console.log(currentOptions[i]);
-    }
+  //sets h2 question to question value in questions object
+  qQuestion.textContent = currentQuestion.question;
+
+  //appends the questions to the question header element
+  qQuestion.appendChild(h2El);
+
+  //creates a button for each question
+  for (var i = 0; i < currentOptions.length; i++) {
+    q[i].textContent = currentOptions[i];
+    qChoices.appendChild(q[i]);
+
+    //adds click event listener to each button
+    q[i].onclick = checkAnswer;
+  }
 }
 
+//checks text content of button and compares to answer in questions object. Then runs appropriate logic
+function checkAnswer() {
+  var feedback = "";
+  var resetTime = 4;
+
+  //creates feedback and deducts points
+  if (this.textContent === questions[questionIndex].answer) {
+    feedback = "Correct!";
+  } else {
+    timeLeft -= 10;
+    feedback = "Incorrect!";
+  }
+
+  feedbackArea.textContent = feedback;
+  feedbackArea.appendChild(pEl);
+  feedbackArea.removeAttribute("class", "hide");
+  questionIndex++;
+
+  getQuestion();
+  resetFeed(resetTime);
+  console.log(questionIndex);
+}
+
+function logHighscores() {
+    var highScores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    console.log(highScores);
+    
+}
 
 //timer function
 function countdown() {
-    var timeLeft = 5;
+  var timeInterval = setInterval(function () {
+    timerEl.textContent = timeLeft;
+    timeLeft--;
 
-    var timeInterval = setInterval(function() {
+    if (timeLeft <= 0) {
+      clearInterval(timeInterval);
+      timerEl.textContent = 0;
+      qScreen.setAttribute("class", "hide");
+      endScreen.removeAttribute("class", "hide");
+      console.log("Game finished");
+      questionIndex = 0;
 
-        timerEl.textContent = timeLeft;
-        timeLeft--;
+      //call function to input name to high scores
+      //bring to highscores page
+      //restart game
+    }
+  }, 1000);
+}
 
-        if(timeLeft === 0){
-            clearInterval(timeInterval);
-            timerEl.textContent = 0;
-            
-            //call function to input name to high scores
-            //bring to highscores page
-            //restart game
-        }
-    }, 1000);
-};
+//Resets feed after interval of time
+function resetFeed(time) {
+  var resetInterval = setInterval(function () {
+    time--;
 
+    if (time === 0) {
+      clearInterval(resetInterval);
+      feedback = "";
+      feedbackArea.setAttribute("class", "hide");
+    }
+  }, 1000);
+}
+
+
+logHighscores();
